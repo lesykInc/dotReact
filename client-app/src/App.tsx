65e59e8b297ed7e2b7ba3d4604ceb4eaa538322a
lesykinc <oleksii.latyk@hotmail.com>
2021-02-04 14:07:30 +0200

    added state React State

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import { cars } from './demo';
 // import CarItem from './CarItem'
 
-function App() {
+class App extends Component{
+  state = {
+    values: []
+  }
+  
+  componentDidMount() {
+    this.setState({
+      values: [{id:4, name: "Value 104"}, {id:2, name: "Value 102"}]
+    })
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {this.state.values.map((value: any) => (
+           <li>{value.name}</li>   
+          ))}
+        </ul>
       </header>
       {/*<ul>*/}
       {/*  {cars.map((car) => (*/}
@@ -28,6 +33,7 @@ function App() {
       {/*</ul>*/}
     </div>
   );
+}
 }
 
 export default App;
