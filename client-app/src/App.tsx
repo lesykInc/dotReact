9d470b82a5b4875f668daf4de7c12e00136cc4dc
lesykinc <oleksii.latyk@hotmail.com>
2021-02-05 12:08:04 +0200

    Added simantic ui

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios'
-// import { cars } from './demo';
-// import CarItem from './CarItem'
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component{
   state = {
@@ -13,7 +12,7 @@ class App extends Component{
   componentDidMount() {
     axios.get('http://localhost:5000/api/values')
         .then((response) => {
-          console.log(response);
+          // console.log(response);
           this.setState({
             values: response.data
           })
@@ -23,23 +22,22 @@ class App extends Component{
 
   render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {this.state.values.map((value: any) => (
-           <li key={value.id}>{value.name}</li>   
-          ))}
-        </ul>
-      </header>
-      {/*<ul>*/}
-      {/*  {cars.map((car) => (*/}
-      {/*      <CarItem car={car}/>*/}
-      {/*  ))}*/}
-      {/*</ul>*/}
+    <div>
+        <Header as='h2' icon>
+            <Icon name='users' />
+            <Header.Subheader>
+                dotReact
+            </Header.Subheader>
+        </Header>
+        <List>
+            {this.state.values.map((value: any) => (
+                <List.Item  key={value.id}>{value.name}</List.Item>
+            ))}
+        </List>
+        
     </div>
   );
-}
+ }
 }
 
 export default App;
