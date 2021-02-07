1f8a98864da9bbbf53a0de777010f15696a19f11
lesykinc <oleksii.latyk@gmail.com>
2021-02-07 17:37:00 +0200

    Added CRUD application

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,7 +1,63 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
 namespace Application.Activities
 {
     public class Create
-    {
+    {   
+        // I'm not actually going to return an activity entity from this command
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+
+            public string Title { get; set; }
         
+            public string Description { get; set; }
+        
+            public string Category { get; set; }
+        
+            public DateTime Date{ get; set; }
+        
+            public string City { get; set; }
+        
+            public string Venue { get; set; }
+        }
+        
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            // in this case Unit like an empty object
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = new Activity
+                {
+                    Id = request.Id,
+                    Title = request.Title,
+                    Description = request.Description,
+                    Category = request.Category,
+                    Date = request.Date,
+                    City = request.City,
+                    Venue = request.Venue
+                };
+                
+                //add activity to this context
+                _context.Activities.Add(activity);
+                
+                // if SaveChanges = 0 then nothing's been saved to db
+                var success = await _context.SaveChangesAsync() > 0;
+                if(success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
     }
 }
