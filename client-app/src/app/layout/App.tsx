358fdbb63128c95c70de8b523fff5143aad80a14
lesykInc <oleksii.latyk@gmail.com>
2021-02-08 12:57:37 +0200

    Added the Navigation bar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Header, Icon, List } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
 
 const App = () => {
     const [activities, setActivities] = useState<IActivity[]>([])
@@ -26,12 +27,7 @@ const App = () => {
 
   return (
     <div>
-        <Header as='h2' icon>
-            <Icon name='users' />
-            <Header.Subheader>
-                dotReact
-            </Header.Subheader>
-        </Header>
+        <NavBar />
         <List>
             {activities.map((activity: any) => (
                 <List.Item  key={activity.id}>{activity.title }</List.Item>
---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Container, Menu, Button } from 'semantic-ui-react'
+
+const NavBar = () => {
+    return (
+        <Menu fixed='top' inverted>
+            <Container>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo"/>
+                    dotReact
+                </Menu.Item>
+                <Menu.Item name='Activities' />
+                <Menu.Item>
+                    <Button positive content='Create Activity' />
+                </Menu.Item>
+            </Container>
+        </Menu> 
+    );
+};
+
+
+export default NavBar
