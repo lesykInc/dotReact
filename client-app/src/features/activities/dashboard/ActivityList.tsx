e235562a606ea968e32f83cd5c644e92d7a941da
lesykinc <oleksii.latyk@gmail.com>
2021-02-12 13:48:55 +0200

    added computed properties by MobX

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -3,12 +3,15 @@ import React, { SyntheticEvent, useContext } from 'react'
 import { Item, Button, Label, Segment } from 'semantic-ui-react';
 import {IActivity} from "../../../app/models/activity";
 import ActivityStore from '../../../app/stores/activityStore'
+import {makeAutoObservable} from 'mobx';
 
 interface IProps {
     deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
     submitting: boolean;
     target: string;
 }
+
+
 
 const ActivityList: React.FC<IProps> = (
     {         deleteActivity,
@@ -17,12 +20,12 @@ const ActivityList: React.FC<IProps> = (
     }) => {
     
     const activityStore = useContext(ActivityStore);
-    const {activities, selectActivity} = activityStore;
+    const {activitiesByDate, selectActivity} = activityStore;
     
     return (
         <Segment clearing>
             <Item.Group divided>
-                {activities.map(activity =>(
+                {activitiesByDate.map(activity =>(
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as='a'>{activity.title}</Item.Header>
