5ee5e818a85f01fb374812b64f2ff76a5ab11be5
lesykInc <oleksii.latyk@gmail.com>
2021-02-22 17:51:06 +0200

    created the Activity Details page

---
+++ client-app/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Item, Button, Segment, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { IActivity } from '../../../app/models/activity';
+
+const ActivityDetailedInfo = () => {
+    return (
+        <div>
+            <h1>Info</h1>
+        </div>
+    );
+};
+
+export default ActivityDetailedInfo;
---
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Item, Button, Segment, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { IActivity } from '../../../app/models/activity';
+
+const ActivityDetailedSidebar = () => {
+    return (
+        <div>
+            <h1>Sidebar</h1>
+        </div>
+    );
+};
+
+export default ActivityDetailedSidebar;
