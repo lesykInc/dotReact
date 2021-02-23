aa711f6e2dbdf0fa71cb2a8e26fcfdd111e46bf3
lesykInc <oleksii.latyk@gmail.com>
2021-02-23 13:19:04 +0200

    added error handling middleware

---
+++ API/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Application.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace API.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandlerExceptionAsync(context, ex, _logger);
+            }
+        }
+
+        private async Task HandlerExceptionAsync(HttpContext context, Exception ex, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            object errors = null;
+
+            switch (ex)
+            {
+                case RestExeption re:
+                    logger.LogError(ex, "REST ERROR");
+                    errors = re.Errors;
+                    context.Response.StatusCode = (int) re.Code;
+                    break;
+                case Exception e:
+                    logger.LogError(ex, "SERVER ERROR");
+                    errors = string.IsNullOrWhiteSpace(e.Message) ? "ERROR" : e.Message;
+                    context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            if (errors != null)
+            {
+                var result = JsonSerializer.Serialize(new
+                {
+                    errors
+                });
+
+                await context.Response.WriteAsync(result);
+            }
+        }
+    }
+}
