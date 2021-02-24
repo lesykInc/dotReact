1e26860ed8935a3fea536b233ed0918796d241c7
lesykInc <oleksii.latyk@gmail.com>
2021-02-24 10:12:23 +0200

    fixed type error

--- Application/Errors/RestExeption.cs
+++ Application/Errors/RestExeption.cs
@@ -3,14 +3,14 @@ using System.Net;
 
 namespace Application.Errors
 {
-    public class RestExeption: Exception
+    public class RestException : Exception
     {
-        public RestExeption(HttpStatusCode code, object errors = null )
+        public RestException(HttpStatusCode code, object errors = null)
         {
             Code = code;
             Errors = errors;
         }
-        
+
         public HttpStatusCode Code { get; }
         public object Errors { get; }
     }
