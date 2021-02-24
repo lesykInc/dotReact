9614cea6ad2d4c5b00b9ca9cc55b046753c91515
lesykInc <oleksii.latyk@gmail.com>
2021-02-24 10:13:53 +0200

    added Newtonsoft.json package

--- API/Middleware/ErrorHandlingMiddleware.cs
+++ API/Middleware/ErrorHandlingMiddleware.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Net;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Application.Errors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace API.Middleware
 {
@@ -12,11 +12,10 @@ namespace API.Middleware
     {
         private readonly RequestDelegate _next;
         private readonly ILogger<ErrorHandlingMiddleware> _logger;
-        
         public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
         {
-            _next = next;
             _logger = logger;
+            _next = next;
         }
 
         public async Task Invoke(HttpContext context)
@@ -24,35 +23,35 @@ namespace API.Middleware
             try
             {
                 await _next(context);
-            }
+            } 
             catch (Exception ex)
             {
-                await HandlerExceptionAsync(context, ex, _logger);
+                await HandleExceptionAsync(context, ex, _logger);
             }
         }
 
-        private async Task HandlerExceptionAsync(HttpContext context, Exception ex, ILogger<ErrorHandlingMiddleware> logger)
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex, ILogger<ErrorHandlingMiddleware> logger)
         {
             object errors = null;
 
             switch (ex)
             {
-                case RestExeption re:
+                case RestException re:
                     logger.LogError(ex, "REST ERROR");
                     errors = re.Errors;
-                    context.Response.StatusCode = (int) re.Code;
+                    context.Response.StatusCode = (int)re.Code;
                     break;
                 case Exception e:
                     logger.LogError(ex, "SERVER ERROR");
-                    errors = string.IsNullOrWhiteSpace(e.Message) ? "ERROR" : e.Message;
-                    context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                    errors = string.IsNullOrWhiteSpace(e.Message) ? "Error" : e.Message;
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                     break;
             }
 
             context.Response.ContentType = "application/json";
             if (errors != null)
             {
-                var result = JsonSerializer.Serialize(new
+                var result = JsonConvert.SerializeObject(new 
                 {
                     errors
                 });
