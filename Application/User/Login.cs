1d17e1d1fa5a6846135ae692dcbd0215af3ef5c2
lesykInc <oleksii.latyk@gmail.com>
2021-02-26 14:43:11 +0200

    added a User object

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -12,7 +12,7 @@ namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -27,18 +27,18 @@ namespace Application.User
             }
         }
     
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
             
             public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
             {
-                _userManager = userManager;
                 _signInManager = signInManager;
+                _userManager = userManager;
             }
 
-            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
 
@@ -51,7 +51,13 @@ namespace Application.User
                 if (result.Succeeded)
                 {
                     // TODO: generate token
-                    return user;
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = "this will be token",
+                        Username = user.UserName,
+                        Image = null
+                    };
                 }
 
                 throw new RestException(HttpStatusCode.Unauthorized);
