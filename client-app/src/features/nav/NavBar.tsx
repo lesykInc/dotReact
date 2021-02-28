3ee40133c0ea7c2ab68ac563a17a6fb69cea853a
lesykInc <oleksii.latyk@gmail.com>
2021-02-28 22:09:54 +0200

    added home page and NavBar user information

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,10 +1,13 @@
 import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
-import {Container, Menu, Button } from 'semantic-ui-react';
+import {Link, NavLink } from 'react-router-dom';
+import {Container, Menu, Button, Dropdown, Image } from 'semantic-ui-react';
+import { RootStoreContext } from '../../app/stores/rootStore';
 
 const NavBar: React.FC = () => {
-
+    
+    const rootStore = useContext(RootStoreContext);
+    const {isLoggedIn, user} = rootStore.userStore;
     return (
         <Menu fixed='top' inverted>
             <Container>
@@ -20,6 +23,22 @@ const NavBar: React.FC = () => {
                     <Menu.Item position={"right"}>
                         <Button as={NavLink} to='/createPost' positive content='Create Post'/>
                 </Menu.Item>
+                {user && (
+                    <Menu.Item position='right'>
+                        <Image avatar spaced='right' src={user.image || '/assets/user.png'} />
+                        <Dropdown pointing='top left' text={user.displayName}>
+                            <Dropdown.Menu>
+                                <Dropdown.Item
+                                    as={Link}
+                                    to={`/profile/username`}
+                                    text='My profile'
+                                    icon='user'
+                                />
+                                <Dropdown.Item text='Logout' icon='power' />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Menu.Item>
+                )}
             </Container>
         </Menu> 
     );
