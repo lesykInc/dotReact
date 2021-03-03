e8d259b052a6efc7a90b5032977528301722ef59
lesykInc <oleksii.latyk@gmail.com>
2021-03-03 10:59:46 +0200

    rerendered the buttons

--- client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Segment, List, Label, Item, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
-import { Profile } from '../../../app/models/profile'
+import { Activity } from '../../../app/models/activity'
 
 interface Props {
-    attendees: Profile[];
+    activity: Activity;   
 }
 
-export default observer(function ActivityDetailedSidebar ({attendees}: Props) {
+export default observer(function ActivityDetailedSidebar ({activity: {attendees, host}}: Props) {
+    if (!attendees) return null;
     return (
         <>
             <Segment
@@ -23,23 +24,24 @@ export default observer(function ActivityDetailedSidebar ({attendees}: Props) {
             </Segment>
             <Segment attached>
                 <List relaxed divided>
-                    {attendees.map(attendees =>(
-                        <Item style={{ position: 'relative' }} key={attendees.username}>
+                    {attendees.map(attendee => (
+                        <Item style={{ position: 'relative' }} key={attendee.username}>
+                            {attendee.username === host?.username &&
                             <Label
                                 style={{ position: 'absolute' }}
                                 color='orange'
                                 ribbon='right'
                             >
                                 Host
-                            </Label>
-                            <Image size='tiny' src={attendees.image || '/assets/user.png'} />
+                            </Label>}
+                            <Image size='tiny' src={attendee.image || '/assets/user.png'} />
                             <Item.Content verticalAlign='middle'>
                                 <Item.Header as='h3'>
-                                    <Link to={`/profiles/${attendees.username}`}>{attendees.displayName}</Link>
+                                    <Link to={`/profiles/${attendee.username}`}>{attendee.displayName}</Link>
                                 </Item.Header>
                                 <Item.Extra style={{ color: 'orange' }}>Following</Item.Extra>
                             </Item.Content>
-                        </Item>  
+                        </Item>
                     ))}
                 </List>
             </Segment>
