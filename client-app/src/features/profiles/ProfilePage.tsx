a125ab4bbe8c7106db60d6b29b804f8926aa8928
lesykInc <oleksii.latyk@gmail.com>
2021-03-04 09:49:36 +0200

    added the profile content

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,14 +1,16 @@
 ﻿import { observer } from 'mobx-react-lite';
 import React  from 'react';
 import { Grid } from 'semantic-ui-react';
+import ProfileContent from './ProfileContent';
 import ProfileHeader from './ProfileHeader';
 
 export default observer(function ProfilePage() {
    
     return (
         <Grid>
-            <Grid.Column width={16}>
+            <Grid.Column width={16}>    
                 <ProfileHeader />
+                <ProfileContent  />
             </Grid.Column>
         </Grid>
     )
