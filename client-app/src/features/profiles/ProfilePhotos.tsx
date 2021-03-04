4b1793c40b4ef7fe2f7b7b2510d1b6c2d902b1fe
lesykInc <oleksii.latyk@gmail.com>
2021-03-04 15:26:18 +0200

    added setting the main photo

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,8 +1,8 @@
 ﻿import { observer } from 'mobx-react-lite';
-import React, { useState } from 'react';
+import React, {SyntheticEvent, useState } from 'react';
 import {Card, Header, Tab, Image, Grid, Button } from 'semantic-ui-react';
 import PhotoUploadWidget from '../../app/common/imageUpload/PhotoUploadWidget';
-import { Profile } from '../../app/models/profile';
+import {Photo, Profile } from '../../app/models/profile';
 import { useStore } from '../../app/stores/store';
 
 interface Props {
@@ -10,21 +10,26 @@ interface Props {
 }
 
 export default observer(function ProfilePhotos({profile}: Props) {
-    const {profileStore: {isCurrentUser, uploadPhoto, uploading}} = useStore();
+    const {profileStore: {isCurrentUser, uploadPhoto, uploading, loading, setMainPhoto}} = useStore();
     const [addPhotoMode, setAddPhotoMode] = useState(false);
+    const [target, setTarget] = useState('');
 
     function handlePhotoUpload(file: Blob) {
         uploadPhoto(file).then(() => setAddPhotoMode(false));
     }
-    
+
+    function handleSetMainPhoto(photo: Photo, e: SyntheticEvent<HTMLButtonElement>) {
+        setTarget(e.currentTarget.name);
+        setMainPhoto(photo);
+    }
+
     return (
         <Tab.Pane>
             <Grid>
                 <Grid.Column width={16}>
-                    <Header floated={"left"} icon={"image"} content={'Photos'} />
+                    <Header floated='left' icon='image' content='Photos' />
                     {isCurrentUser && (
-                        <Button floated={"right"} 
-                                basic 
+                        <Button floated='right' basic
                                 content={addPhotoMode ? 'Cancel' : 'Add Photo'}
                                 onClick={() => setAddPhotoMode(!addPhotoMode)}
                         />
@@ -37,10 +42,32 @@ export default observer(function ProfilePhotos({profile}: Props) {
                         <Card.Group itemsPerRow={5}>
                             {profile.photos?.map(photo => (
                                 <Card key={photo.id}>
-                                    <Image scr={photo.url }/>
+                                    <Image src={photo.url} />
+                                    {isCurrentUser && (
+                                        <Button.Group fluid widths={2}>
+                                            <Button
+                                                basic
+                                                color='green'
+                                                content='Main'
+                                                name={'main' + photo.id}
+                                                disabled={photo.isMain}
+                                                loading={target === 'main' + photo.id && loading}
+                                                onClick={e => handleSetMainPhoto(photo, e)}
+                                            />
+                                            <Button
+                                                basic
+                                                color='red'
+                                                icon='trash'
+                                                loading={target === photo.id && loading}
+                                                // onClick={e => handleDeletePhoto(photo, e)}
+                                                disabled={photo.isMain}
+                                                name={photo.id}
+                                            />
+                                        </Button.Group>
+                                    )}
                                 </Card>
                             ))}
-                        </Card.Group>    
+                        </Card.Group>
                     )}
                 </Grid.Column>
             </Grid>
