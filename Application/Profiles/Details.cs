8dc8c2a0455fa44e551dcb101b6cc75214fb2004
lesykInc <oleksii.latyk@gmail.com>
2021-03-05 15:45:51 +0200

    Updating the other handlers with the following property

--- Application/Profiles/Details.cs
+++ Application/Profiles/Details.cs
@@ -1,12 +1,12 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-
 namespace Application.Profiles
 {
     public class Details
@@ -15,13 +15,15 @@ namespace Application.Profiles
         {
             public string Username { get; set; }
         }
-
         public class Handler : IRequestHandler<Query, Result<Profile>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-            public Handler(DataContext context, IMapper mapper)
+   
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
             }
@@ -29,7 +31,8 @@ namespace Application.Profiles
             public async Task<Result<Profile>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _context.Users
-                    .ProjectTo<Profile>(_mapper.ConfigurationProvider)
+                    .ProjectTo<Profile>(_mapper.ConfigurationProvider, 
+                        new {currentUsername = _userAccessor.GetUsername()})
                     .SingleOrDefaultAsync(x => x.Username == request.Username);
 
                 if (user == null) return null;
