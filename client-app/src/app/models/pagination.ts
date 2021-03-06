4ee8dd3c9a856713ff66aa14fe2129dd39397109
lesykInc <oleksii.latyk@gmail.com>
2021-03-06 17:34:39 +0200

    added vertical paging

--- client-app/src/app/models/pagination.ts
+++ client-app/src/app/models/pagination.ts
@@ -19,8 +19,8 @@ export class PagingParams {
     pageNumber;
     pageSize;
 
-    constructor(pageNumber = 1, pageSize = 2) {
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-    }
+    constructor(pageNumber = 1, pageSize = 2) {  
+        this.pageNumber = pageNumber;  
+        this.pageSize = pageSize;  
+    }  
 }
