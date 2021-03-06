8683e226569d0226e44efc1c5ce96e11eb3fd29f
lesykInc <oleksii.latyk@gmail.com>
2021-03-06 19:36:00 +0200

    added Scroll to top for our routes

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -10,13 +10,15 @@ import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { Router } from 'react-router-dom';
 import {createBrowserHistory} from 'history';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 export const history = createBrowserHistory();
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
     <Router history={history}>
-      <App />
+        <ScrollToTop />
+        <App />
     </Router>
   </StoreContext.Provider>,
   document.getElementById('root')
